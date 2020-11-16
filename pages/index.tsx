29d51d51cc2d9ab90112a9855ffb02014e7a912d
Sebastian Pawlak <sebastian.pawlak-c@wpengine.com>
2020-11-16 12:57:57 +0100

    add await

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,11 @@
 const Home = () => <h2>Test app</h2>;
 
+export const getStaticProps = async () => {
+    console.log('fetching data')
+    await new Promise(r => setTimeout(r, 20000))
+    return {
+        props: {},
+    }
+}
+
 export default Home;
