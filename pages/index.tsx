4c32d5931e174809afb4651b7695d119f4913b40
Sebastian Pawlak <sebastian.pawlak-c@wpengine.com>
2020-11-16 12:04:26 +0100

    await

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 const Home = () => <h2>Test app</h2>;
 
 async function waitAMoment() {
-await new Promise(r => setTimeout(r, 20000));
+    await new Promise(r => setTimeout(r, 20000));
 }
 
-waitAMoment()
+waitAMoment();
 
 export default Home;
